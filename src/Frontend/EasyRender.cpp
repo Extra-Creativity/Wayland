a49ad1839e02f1ded64784e404f60b5af2258e8c
Bingxuan_Li@PKU <lbx@stu.pku.edu.cn>
2024-05-10 04:42:07 +0800

    More more more scenes

--- src/Frontend/EasyRender.cpp
+++ src/Frontend/EasyRender.cpp
@@ -16,15 +16,20 @@ const std::string sceneSrc =
     //R"(..\..\..\scene\dragon\dragon-metal-black.pbrt)";
     //R"(..\..\..\scene\veach-mis\mis-lambert.pbrt)";
     //R"(..\..\..\scene\veach-mis\mis.pbrt)";
-     R"(..\..\..\scene\veach-bidir\bidir.pbrt)";
+    R"(..\..\..\scene\veach-bidir\bidir.pbrt)";
+    //R"(..\..\..\scene\water-caustic\water.pbrt)";
+    //R"(..\..\..\scene\veach-ajar\ajar.pbrt)";
+     //R"(..\..\..\scene\staircase\staircase.pbrt)";
+     //R"(..\..\..\scene\glass-of-water\glass-of-water.pbrt)";
+     //R"(..\..\..\scene\kitchen\kitchen.pbrt)";
+
 
 int main(int argc, char **argv)
 {
     ProgramType program = ProgramType::PathTracing;
     try
     {
-        EasyRender::Renderer app({ 1152, 864 }, program, sceneSrc);
-        //EasyRender::Renderer app({ 1200, 1000 }, program, sceneSrc);
+        EasyRender::Renderer app({ 1000, 800 }, program, sceneSrc);
         app.Run();
     }
     catch (const std::exception &e)
